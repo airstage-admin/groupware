2e823b69cf2ec26ae31b3b1d1edef5bfd8c0c509
simd-float4x4 <asobu.developers+mathew@gmail.com>
2025-11-28 13:59:56 +0900

    create: 邨瑚ｲｻ邊ｾ邂苓ｩｳ邏ｰ逕ｻ髱｢・・nternal_cost_detail.html・峨→Controller繧ｨ繝ｳ繝峨・繧､繝ｳ繝医・霑ｽ蜉

--- src/main/java/com/groupware/costs/controller/CostsController.java
+++ src/main/java/com/groupware/costs/controller/CostsController.java
@@ -18,6 +18,7 @@ import org.springframework.ui.Model;
 //import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 //import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -50,5 +51,35 @@ public class CostsController {
 		}
 		model.addAttribute("user", loginUser);
 		return "internal_cost/internal_cost_home";
+	}
+
+	/**
+	* 経費精算詳細画面表示処理（GET用）
+	*
+	* @param id 経費ID
+	* @param session セッション
+	* @param model モデル
+	* @return 詳細画面
+	*/
+	@GetMapping("/costs/detail/{id}")
+	public String getCostDetail(@PathVariable("id") String id, HttpSession session, Model model) {
+		UserDto loginUser = (UserDto) session.getAttribute("loginUser");
+		if (loginUser == null) {
+			return "redirect:/index";
+		}
+		model.addAttribute("user", loginUser);
+		
+		// TODO: 実際のデータはDBから取得する
+		// 仮データをセット
+		model.addAttribute("costId", id);
+		model.addAttribute("status", "rejected"); // pending, approved, rejected
+		model.addAttribute("rejectReason", "経費として認められないため。");
+		model.addAttribute("accountTitle", "旅費交通費");
+		model.addAttribute("subCategory", "常駐先の会社に対する交通費（クライアント承認済）");
+		model.addAttribute("amount", "620");
+		model.addAttribute("purpose", "〇〇〇会社出勤時の地下鉄代（高田馬場～六本木一丁目駅）");
+		model.addAttribute("purchaseDate", "2025/11/14");
+		
+		return "internal_cost/internal_cost_detail";
 	}
 }
