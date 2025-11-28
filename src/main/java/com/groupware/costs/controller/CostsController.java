ae73f9ce0d892697d2a6fc803651908dd629ebdf
simd-float4x4 <asobu.developers+mathew@gmail.com>
2025-11-28 12:12:36 +0900

    fix: ヘッダーにユーザー名を適用

--- src/main/java/com/groupware/costs/controller/CostsController.java
+++ src/main/java/com/groupware/costs/controller/CostsController.java
@@ -6,13 +6,13 @@ package com.groupware.costs.controller;
 //import java.util.List;
 //import java.util.regex.Pattern;
 //
-//import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSession;
 //
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
+import org.springframework.ui.Model;
 //import org.springframework.validation.BindingResult;
 //import org.springframework.validation.annotation.Validated;
 //import org.springframework.web.bind.annotation.CookieValue;
@@ -22,25 +22,33 @@ import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 //import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.groupware.dto.UserDto;
+
 @Controller
 public class CostsController {
 	/**
-	* 社員用勤怠カレンダ一覧表示処理（GET用）
+	* 一般ユーザー・経費精算ホーム画面表示処理（POST用）
 	*
-	* @param　selectedMonthYear　対象年月クッキー値
-	* @param　placeCategoryCookieValue　勤務先クッキー値
-	* @param　session セッション
-	* @param　model モデル
-	* @param　redirectAttributes　表示されるデータ
-	* @return　
 	*/
 	@PostMapping("/costs/list")
 	public String postCosts() {
 		return "redirect:/costs/list";
 	}
 
+	/**
+	* 一般ユーザー・経費精算ホーム画面表示処理（GET用）
+	*
+	* @param　session セッション
+	* @param　model モデル
+	* @return　
+	*/
 	@GetMapping("/costs/list")
-	public String getCosts() {
+	public String getCosts(HttpSession session, Model model) {
+		UserDto loginUser = (UserDto) session.getAttribute("loginUser");
+		if (loginUser == null) {
+			return "redirect:/index";
+		}
+		model.addAttribute("user", loginUser);
 		return "internal_cost/internal_cost_home";
 	}
 }
